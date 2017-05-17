ea55de6a5d3e75021cf7c21d1d7da108905498a2
clwater <gengzhibo@gengzhibo.net>
2017-05-17 22:32:59 +0800

    20170517

--- LAL/ZhiJi/app/src/main/java/com/clwater/zhiji/ui/activity/EditActivity.java
+++ LAL/ZhiJi/app/src/main/java/com/clwater/zhiji/ui/activity/EditActivity.java
@@ -3,7 +3,13 @@ package com.clwater.zhiji.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 import com.clwater.zhiji.R;
 import com.rengwuxian.materialedittext.MaterialEditText;
@@ -18,12 +24,9 @@ import ren.qinc.edit.PerformEdit;
  */
 
 public class EditActivity extends AppCompatActivity {
-    @BindView(R.id.editText_edit_text)
-    MaterialEditText editText_edit_text;
-
-    @BindView(R.id.id1) Button button1;
-    @BindView(R.id.id2) Button button2;
-    @BindView(R.id.id3) Button button3;
+    @BindView(R.id.editText_edit_text) MaterialEditText editText_edit_text;
+    @BindView(R.id.editText_edit_title) MaterialEditText editText_edit_title;
+    @BindView(R.id.view_edit_spaceview) View view_edit_spaceview;
 
     PerformEdit mPerformEdit;
 
@@ -31,6 +34,8 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
 
         ButterKnife.bind(this);
 
@@ -39,24 +44,59 @@ public class EditActivity extends AppCompatActivity {
 
     private void init() {
         mPerformEdit = new PerformEdit(editText_edit_text);
-        mPerformEdit.setDefaultText("这是初始值,不做撤销记录");
-        
+        mPerformEdit.setDefaultText("");
+
+
+        init_editText_edit_text();
 
     }
 
-    @OnClick(R.id.id1)
-    public void b1(){
-        mPerformEdit.undo();
+    private void init_editText_edit_text() {
+
+        editText_edit_text.setOnFocusChangeListener(new android.view.View.
+                OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                RelativeLayout.LayoutParams relaParams = (RelativeLayout.LayoutParams)  view_edit_spaceview.getLayoutParams();
+                if (hasFocus) {
+                    relaParams.height = 0;
+                } else {
+                    if(editText_edit_text.getHeight() < 1000) {
+                        relaParams.height = 500;
+                    }
+                }
+                view_edit_spaceview.setLayoutParams(relaParams);
+
+            }
+        });
     }
 
-    @OnClick(R.id.id2)
-    public void b2(){
-        mPerformEdit.redo();
+    @OnClick(R.id.view_edit_spaceview)
+    public void view_edit_spaceview_onclick(){
+        editText_edit_text.setFocusable(true);
+        editText_edit_text.setFocusableInTouchMode(true);
+        editText_edit_text.requestFocus();
     }
 
-    @OnClick(R.id.id3)
-    public void b3(){
-        mPerformEdit.clearHistory();
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_edit, menu);
+        return super.onCreateOptionsMenu(menu);
     }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuitem_edit_undo:
+                mPerformEdit.undo();
+                return true;
+            case R.id.menuitem_edit_redo:
+                mPerformEdit.redo();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
 }
